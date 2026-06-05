b92a4772112d3fba1ffc6d0ba516c9f6f0282b5e
nightcelestial <nightcelestial@gmail.com>
2026-06-05 10:00:27 +0700

    refactor: Adding App Icon

---
+++ ParkHere/Core/DesignSystem/AppIcon.swift
@@ -0,0 +1,18 @@
+//
+//  AppIcon.swift
+//  ParkHere
+//
+//  Created by Kelly Angeline on 05/06/26.
+//
+
+import SwiftUI
+
+enum AppIcon {
+    static let questionMark = "questionmark"
+    static let mapPin = "mappin"
+    static let mapPinCircleFill = "mappin.circle.fill"
+    static let xMark = "xmark"
+    static let flip = "arrow.trianglehead.2.clockwise.rotate.90"
+    static let refresh = "arrow.trianglehead.clockwise"
+
+}
